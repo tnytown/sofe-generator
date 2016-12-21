6f7a0be6f69d4053f551d10e84226dea33ea2c3a
KnownUnown <andrew.tianyi.pan@gmail.com>
2016-12-21 08:59:12 -0600

    Does this help?

--- main.go
+++ main.go
@@ -3,11 +3,12 @@ package main
 import (
 	"fmt"
 	"net/http"
+	"os"
 )
 
 func main() {
 	http.HandleFunc("/", generate)
-	http.ListenAndServe(":8080", nil)
+	http.ListenAndServe(":"+os.Getenv("HTTP_PLATFORM_PORT"), nil)
 }
 
 func generate(w http.ResponseWriter, r *http.Request) {
